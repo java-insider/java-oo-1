b68862c42e6293317396efbece1918abd3fdffdc
Carlos Tosin <carlos.tosin@pixiongames.com>
2022-08-19 09:33:28 +0100

    Code

---
+++ br/com/javainsider/app/App11.java
@@ -0,0 +1,13 @@
+package br.com.javainsider.app;
+
+import static java.lang.Math.*;
+
+public class App11 {
+
+    public static void main(String[] args) {
+
+        double n = pow(3, 2);
+        double pi = PI;
+        double x = abs(-10);
+    }
+}
